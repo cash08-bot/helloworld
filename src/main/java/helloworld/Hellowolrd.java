0e7f2486343873fb87873af0fc8383d13727046b
Akashreddy Muduganti <amuduganti@leveragesoft.com>
2025-05-13 10:15:35 -0400

    Initial commit from Eclipse project

---
+++ src/main/java/helloworld/Hellowolrd.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class Hellowolrd {
+
+	public static void main(String[] args) {
+		 System.out.println("Hello from ADO ");
+		// TODO Auto-generated method stub
+
+	}
+
+}
